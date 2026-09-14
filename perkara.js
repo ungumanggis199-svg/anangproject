349176dd26495f0a76a08bc98c0888b0024d5302
ungumanggis199-svg <ungumanggis199@gmail.com>
2026-09-14 09:59:00 +0800

    Update perkara.js

--- perkara.js
+++ perkara.js
@@ -364,7 +364,7 @@ function renderPrkTable(data) {
     return;
   }
 
-  const PRK_LIST_LIMIT = 4;
+  const PRK_LIST_LIMIT = 3;
   const shown = data.slice(0, PRK_LIST_LIMIT);
   const sisa  = data.length - shown.length;
 
